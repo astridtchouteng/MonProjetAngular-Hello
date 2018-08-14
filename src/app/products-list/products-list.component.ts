8b4fa85fdbdb8d8ccd8950949f957a1c96a336c3
Astrid TCHOUTENG <astrid.tchouteng@gmail.com>
2018-08-14 14:10:44 +0200

    Ajout du composant produitsList

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsListComponent implements OnInit {
 
-  constructor() { }
+  data: any[] ;
+  constructor() {
+   }
 
   ngOnInit() {
+    this.data = [
+      {'code': 'P100', 'titre': 'Café', 'prixUnitaire': 5.5} ,
+      {'code': 'P200', 'titre': 'Thé', 'prixUnitaire': 2.5} ,
+      {'code': 'P300', 'titre': 'Lait', 'prixUnitaire': 4.5} ,
+    ];
   }
 
 }
